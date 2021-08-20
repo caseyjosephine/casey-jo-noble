c755d295e91ef452290d8b9b09000019269be3f5
Casey Noble <caseynoble@Starkiller-Base.local>
2021-08-19 18:14:51 -0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About.">
+    <p>Casey loves wine, duck fat, and learning new things. She works part-time as a Jedi Knight.</p>
+    </Layout>
+    )
+}
+
+export default AboutPage
