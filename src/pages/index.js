043073186989e3ef7895746d291d142fd99e9abc
Casey Noble <caseynoble@Starkiller-Base.local>
2021-08-25 19:34:38 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home.">
       <p>Welcome to my personal website.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/casey-jo-noble-napa-wine-pro.jpg"
+      />
     </Layout>
   )
 }
